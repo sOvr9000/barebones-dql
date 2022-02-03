fcd70c0300dbf1a84e1df9359f34617ace5c6e86
sOvr9000 <jordanthegeek@gmail.com>
2022-02-02 17:38:03 -0800

    Implement basic DQL algorithm

--- dql.py
+++ dql.py
@@ -68,17 +68,66 @@ def build_model():
 model = build_model()
 env = SuperSimpleEnvironment()
 
+old_states = []
+new_states = []
+actions = []
+rewards = []
+
 print(env.position)
-for i in range(20):
+while True:
     print('-'*57)
     action = np.argmax(model.predict(np.expand_dims(env.position, 0)))
     # Keras models assume there's a batch dimension, so the position is nested in another array so that it is at index 0 along the batch dimension.
-    print(f'Move by {action:+}')
+    print(f'Move by {action-2:+}')
+    old_state = env.position.copy()
     env.step(action - 2)
     # Subtract 2 so that the 0th action is -2, 1st action is -1, 2nd action is 0, 3rd action is 1, and 4th action is 2.
+    new_state = env.position.copy()
     print(env.position)
     reward = get_reward(env.position)
+    # Eventually, the reward received will consistently be 0.5 and 1.0, after sufficient training.
     print(f'Received reward {reward:.4f}')
+
+    # Save the transition from old_state to new_state, which includes information about the reward received by taking some action.
+    old_states.append(old_state)
+    new_states.append(new_state)
+    actions.append(action)
+    rewards.append(reward)
+
+    # Perform experience replay.
+    # This is where the model trains on a random batch of transitions that have been saved.
+
+    # First, gather the random batch of transitions.
+    transition_indices = np.random.randint(0, len(old_states), size=16)
+    sample_old_states = np.array(old_states)[transition_indices]
+    sample_new_states = np.array(new_states)[transition_indices]
+    sample_actions = np.array(actions)[transition_indices]
+    sample_rewards = np.array(rewards)[transition_indices]
+
+    # Next, use the update rule to train the model on its own predictions, but with one of them slightly more accurate than before.
+    pred_old_states = model.predict(sample_old_states)
+    pred_new_states = model.predict(sample_new_states)
+    pred_old_states[np.arange(16),sample_actions] = sample_rewards + 0.98 * np.max(pred_new_states)
+
+    model.fit(sample_old_states, pred_old_states, verbose=False)
+
+
+
+###############################################################
+# model = build_model()
+# env = SuperSimpleEnvironment()
+
+# print(env.position)
+# for i in range(20):
+#     print('-'*57)
+#     action = np.argmax(model.predict(np.expand_dims(env.position, 0)))
+#     # Keras models assume there's a batch dimension, so the position is nested in another array so that it is at index 0 along the batch dimension.
+#     print(f'Move by {action:+}')
+#     env.step(action - 2)
+#     # Subtract 2 so that the 0th action is -2, 1st action is -1, 2nd action is 0, 3rd action is 1, and 4th action is 2.
+#     print(env.position)
+#     reward = get_reward(env.position)
+#     print(f'Received reward {reward:.4f}')
 
 
 
